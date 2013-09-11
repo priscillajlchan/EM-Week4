34a27b20b087684b46806f18a889a4c6a9217251
Ninjaparade <yazjallad@gmail.com>
2013-09-11 10:34:50 -0700

    Week four fixed

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($) {
+	$('.dropdown li a').hover(function(event) {
+		// Stuff to do when the mouse enters the element;
+
+	}, function() {
+		// Stuff to do when the mouse leaves the element;
+	});
+});
